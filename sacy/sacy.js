80e88d0940547e88af74f117e6ec49ebfe085273
paulodinizz <paulodiniz7001@gmail.com>
2022-11-23 00:39:42 -0300

    create system to copy and change image

--- sacy/sacy.js
+++ sacy/sacy.js
@@ -14,3 +14,59 @@ options.forEach(option =>{
         optionMenu.classList.remove("active");
     })
 })
+
+function imgMira1(){
+   var img = document.getElementById('imageContentChange');
+   img.setAttribute('src', 'images/crosshair/crosshair1.png')
+   document.getElementById("importMira").innerHTML = "0;P;h;0;f;0;0l;4;0o;0;0a;1;0f;0;1b;0";
+}
+function imgMira2(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair2.png')
+    document.getElementById("importMira").innerHTML = "Teste 2";
+}
+function imgMira3(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair3.png')
+    document.getElementById("importMira").innerHTML = "Teste 3";
+}
+function imgMira4(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair4.png')
+    document.getElementById("importMira").innerHTML = "Teste 4";
+}
+function imgMira5(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair5.png')
+    document.getElementById("importMira").innerHTML = "Teste 5";
+}
+function imgMira6(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair6.png')
+    document.getElementById("importMira").innerHTML = "Teste 6";
+}
+function imgMira7(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair7.png')
+    document.getElementById("importMira").innerHTML = "Teste 7";
+}
+function imgMira8(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair8.png')
+    document.getElementById("importMira").innerHTML = "Teste 8";
+}
+function imgMira9(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair9.png')
+    document.getElementById("importMira").innerHTML = "Teste 9";
+}
+function imgMira10(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair10.png')
+    document.getElementById("importMira").innerHTML = "Teste 10";
+}
+function imgMira11(){
+    var img = document.getElementById('imageContentChange');
+    img.setAttribute('src', 'images/crosshair/crosshair11.png')
+    document.getElementById("importMira").innerHTML = "Teste 11";
+}
